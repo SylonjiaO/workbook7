226817c5f46c215a40f551ce53f383cf191b8d30
Sylonjia Odion <Sodion@atl.yearup.org>
2022-11-09 15:59:11 -0500

    done

---
+++ practice.js
@@ -0,0 +1,30 @@
+// function declaration
+function hello (name){};
+
+//function expression
+const hello = (name) =>{};
+
+const hello = function (name) =>{};
+
+//arrow function
+(first) => {
+    Script;
+}
+
+//object
+let animal = {
+    name:'Bird',
+    hasFur: false
+}
+
+//array
+let numbers= [1,2,3,4,5]
+
+let courses = [
+    {coursework: 'get'},
+    {coursework: 'get'},
+    {coursework: 'get'},
+
+]
+
+
